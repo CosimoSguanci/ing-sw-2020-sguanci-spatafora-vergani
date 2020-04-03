fd22d744604dc4babbac7d2e4a7fca3dde132272
RobertoSpatafora <61706995+RobertoSpatafora@users.noreply.github.com>
2020-04-03 16:05:26 +0200

    comments created

--- src/main/java/it/polimi/ingsw/controller/CommandType.java
+++ src/main/java/it/polimi/ingsw/controller/CommandType.java
@@ -1,5 +1,12 @@
 package it.polimi.ingsw.controller;
 
+/**
+ * CommandType is the enumeration for the type of command a player can do.
+ * In Santorini there are three type of possible commands: MOVE used to
+ * move a player's worker to a different cell; BUILD used to build in a cell
+ * according to relatives restrictions, ENDTURN to declare the termination
+ * of the turn and then the turn pass to another player.
+ */
 public enum CommandType {
 
     MOVE, BUILD, ENDTURN;
--- src/main/java/it/polimi/ingsw/controller/PlayerCommand.java
+++ src/main/java/it/polimi/ingsw/controller/PlayerCommand.java
@@ -4,14 +4,22 @@ import it.polimi.ingsw.model.Cell;
 import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.model.Worker;
 
+/**
+ * In this class there are references to the player who has playing
+ * the current turn, the type of command requested from the player,
+ * a reference to the worker the player wants it to move or build and
+ * the cell in which the player wants the worker to move in or build in.
+ */
 public class PlayerCommand {
-
     final Player player;
     final CommandType commandType;
     final Worker worker;
     final Cell cell;
 
-
+    /**
+     * PlayerCommand is the builder of the class. Taken as parameters
+     * the builder sets the class' attributes to the relatives values received.
+     */
     public PlayerCommand(Player player, CommandType commandType, Worker worker, Cell cell) {
         this.player = player;
         this.commandType = commandType;
