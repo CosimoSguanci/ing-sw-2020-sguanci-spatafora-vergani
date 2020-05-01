51c70ff3cc9f92f6fad1136398bad56b1ef91912
Cosimo Sguanci <cosimo.sguanci@mail.polimi.it>
2020-05-01 20:24:58 +0200

    Some updates in CLI board test

--- src/main/java/it/polimi/ingsw/ServerApp.java
+++ src/main/java/it/polimi/ingsw/ServerApp.java
@@ -15,32 +15,31 @@ public class ServerApp {
             System.exit(0);
         }
 
-        /*
 
 
-        Match match = Match.getInstance("AAA", 2);
+
+        /*Match match = Match.getInstance("AAA", 2);
         Board gameBoard = match.getMatchBoard();
         Player player1 = new Player("RobS", match);
         Player player2 = new Player("CosS", match);
 
-        player1.setColor(PrintableColour.GREEN);
-        player2.setColor(PrintableColour.BLUE);
+        player1.setColor(PrintableColour.PURPLE);
+        player2.setColor(PrintableColour.YELLOW);
 
-        player1.getWorkerFirst().setInitialPosition(1,0);
-        player1.getWorkerSecond().setInitialPosition(2,3);
-        player2.getWorkerFirst().setInitialPosition(2,0);
-        player2.getWorkerSecond().setInitialPosition(2,2);
+        player1.getWorkerFirst().setInitialPosition(0,0);
+        player1.getWorkerSecond().setInitialPosition(1,0);
+        player2.getWorkerFirst().setInitialPosition(0,1);
+        player2.getWorkerSecond().setInitialPosition(1,1);
 
         char rowIdentifier = 'A';
 
-        System.out.println("\t    1         2         3         4         5    ");
         for (int i = 0; i < 5; i++) {    //Single cell printed as 5x5: +---+ board; " "/"1"/"2" if worker is inside; BlockType specified.
             System.out.println("\t+  -  -  -  +  +  -  -  -  +  +  -  -  -  +  +  -  -  -  +  +  -  -  -  +");
-            System.out.println("\t|        " + convertBlockTypeToUnicode(gameBoard.getCell(i, 0).getLevel()) + "  | "+
-                    " |        " + convertBlockTypeToUnicode(gameBoard.getCell(i, 1).getLevel()) + "  | " +
-                    " |        " + convertBlockTypeToUnicode(gameBoard.getCell(i, 2).getLevel()) + "  | " +
-                    " |        " + convertBlockTypeToUnicode(gameBoard.getCell(i, 3).getLevel()) + "  | " +
-                    " |        " + convertBlockTypeToUnicode(gameBoard.getCell(i, 4).getLevel()) + "  |");
+            System.out.println("\t|         " + convertBlockTypeToUnicode(gameBoard.getCell(i, 0).getLevel()) + " | " +
+                                " |         " + convertBlockTypeToUnicode(gameBoard.getCell(i, 1).getLevel()) + " | " +
+                                " |         " + convertBlockTypeToUnicode(gameBoard.getCell(i, 2).getLevel()) + " | " +
+                                " |         " + convertBlockTypeToUnicode(gameBoard.getCell(i, 3).getLevel()) + " | " +
+                                " |         " + convertBlockTypeToUnicode(gameBoard.getCell(i, 4).getLevel()) + " | ");
 
             System.out.print(rowIdentifier + "\t");
             rowIdentifier++;
@@ -50,14 +49,18 @@ public class ServerApp {
                 if (!gameBoard.getCell(i, j).isEmpty()) {
                     Worker printableWorker = gameBoard.getCell(i, j).getWorker();
                     if (printableWorker.equals(printableWorker.player.getWorkerFirst())) {
-                        System.out.print(convertColorToAnsi(printableWorker.player.getColor()) + " W1" + PrintableColour.RESET);
+                        System.out.print(convertColorToAnsi(printableWorker.player.getColor()) + "\u265F 1" + PrintableColour.RESET);
                     } else {
-                        System.out.print(convertColorToAnsi(printableWorker.player.getColor()) + " W2" + PrintableColour.RESET);
+                        System.out.print(convertColorToAnsi(printableWorker.player.getColor()) + "\u265F 2" + PrintableColour.RESET);
                     }
                 } else {
                     System.out.print("   ");
                 }
+
+
+
                 System.out.print("    |  ");
+
             }
 
             System.out.println();
@@ -66,21 +69,27 @@ public class ServerApp {
 
         }
 
+        System.out.println("\t    1         2         3         4         5    ");*/
+
+
+
+
+
 
     }
 
     private static String convertBlockTypeToUnicode(BlockType level) {
         switch (level) {
             case GROUND:
-                return "\u2070";
+                return "0";
             case LEVEL_ONE:
-                return "\u00B9";
+                return "1";
             case LEVEL_TWO:
-                return "\u00B2";
+                return "2";
             case LEVEL_THREE:
-                return "\u00B3";
+                return "3";
             case DOME:
-                return "\u2074";
+                return "D";
             default:
                 throw new IllegalArgumentException();
         }
@@ -95,13 +104,13 @@ public class ServerApp {
             case GREEN:
                 return "\u001B[32m";
             case YELLOW:
-                return "\u001B[43m";
+                return "\u001B[33m";
             case PURPLE:
                 return "\u001B[35m";
             default:
                 throw new IllegalArgumentException();
         }
         
-         */
+
     }
 }
