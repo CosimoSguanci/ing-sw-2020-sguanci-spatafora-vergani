89222f95d4b77052e899a40a68fa535a13c5668b
Cosimo Sguanci <cosimo.sguanci@mail.polimi.it>
2020-05-16 19:42:34 +0200

    MultipleConcurrencyTest

---
+++ src/test/java/it/polimi/ingsw/controller/MultipleConcurrencyTest.java
@@ -0,0 +1,36 @@
+package it.polimi.ingsw.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.TestPlan;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
+
+public class MultipleConcurrencyTest {
+
+    SummaryGeneratingListener listener = new SummaryGeneratingListener();
+
+    @Disabled
+    @Test
+    public void runAll() {
+        while(true) {
+            runOne();
+        }
+    }
+
+
+    public void runOne() {
+        LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+                .selectors(selectClass(ControllerConcurrencyTest.class))
+                .build();
+        Launcher launcher = LauncherFactory.create();
+        TestPlan testPlan = launcher.discover(request);
+        launcher.registerTestExecutionListeners(listener);
+        launcher.execute(request);
+    }
+}
