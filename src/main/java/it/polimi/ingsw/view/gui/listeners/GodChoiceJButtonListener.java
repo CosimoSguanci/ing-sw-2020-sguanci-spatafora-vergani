b87e2e5a758bcc54ecbdb91bdd048d2376a07c58
RobertoSpatafora <61706995+RobertoSpatafora@users.noreply.github.com>
2020-05-18 22:34:31 +0200

    GodChoice and GodChoiceJButtonListener created

---
+++ src/main/java/it/polimi/ingsw/view/gui/listeners/GodChoiceJButtonListener.java
@@ -0,0 +1,21 @@
+package it.polimi.ingsw.view.gui.listeners;
+
+import it.polimi.ingsw.view.gui.components.GodChoice;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GodChoiceJButtonListener implements ActionListener {
+    private GodChoice godChoice;
+
+    public GodChoiceJButtonListener (GodChoice godChoice) {
+        this.godChoice = godChoice;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton btn = (JButton) e.getSource();
+        godChoice.setGodChoiceSelected(btn);
+    }
+}
