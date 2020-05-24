cac5286b9b24cc9e1f94cbb95d058a2fec2dc383
Cosimo Sguanci <cosimo.sguanci@mail.polimi.it>
2020-05-24 19:59:21 +0200

    added loading "Waiting for a Match" GUI

--- src/main/java/it/polimi/ingsw/view/gui/components/WaitingForAMatch.java
+++ src/main/java/it/polimi/ingsw/view/gui/components/WaitingForAMatch.java
@@ -1,20 +1,47 @@
 package it.polimi.ingsw.view.gui.components;
 
+import it.polimi.ingsw.view.gui.Gui;
+
 import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
 public class WaitingForAMatch extends JPanel {
     private String standardImgPath = "src/main/resources/images/WaitingForAMatch/";
     private Image backgroundImage = new ImageIcon(standardImgPath + "title_FG_grass.png").getImage();
-    private Font font = new Font(Font.SERIF, Font.BOLD, 16);
+    private Font font = Gui.getFont(Gui.FONT_BOLD, 16);
 
     public WaitingForAMatch() {
         LayoutManager layoutManager = new BorderLayout();
         this.setLayout(layoutManager);
+
+        JPanel jPanel = new JPanel();
+        LayoutManager panelLayoutManager = new BoxLayout(jPanel, BoxLayout.Y_AXIS);
+        jPanel.setLayout(panelLayoutManager);
+
         JLabel label = new JLabel("Waiting for a match...");
         label.setFont(this.font);
         label.setHorizontalAlignment(JLabel.CENTER);
-        this.add(label, BorderLayout.NORTH);
+
+        label.setAlignmentX(Component.CENTER_ALIGNMENT);
+        jPanel.add(label);
+
+        Icon imgIcon = new ImageIcon("src/main/resources/loading.gif");
+        JLabel label2 = new JLabel(imgIcon);
+        label2.setBorder(new EmptyBorder(15, 0, 0, 0));
+
+        label2.setAlignmentX(Component.CENTER_ALIGNMENT);
+        jPanel.add(label2);
+
+        jPanel.setOpaque(false);
+
+
+
+        this.add(jPanel, BorderLayout.NORTH);
+
+
+
 
         this.setBorder(BorderFactory.createEmptyBorder(35,20,20,20));
     }
