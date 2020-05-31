e934f18847dc6f8034ef24e97cbb0282e1016e16
RobertoSpatafora <61706995+RobertoSpatafora@users.noreply.github.com>
2020-05-31 15:08:54 +0200

    Update Cli.java

--- src/main/java/it/polimi/ingsw/view/cli/Cli.java
+++ src/main/java/it/polimi/ingsw/view/cli/Cli.java
@@ -53,7 +53,9 @@ public class Cli extends View implements Observer<Update> {
 
     /**
      * Cli is the builder of the class. At the moment of the Cli creation
-     * a client and a controller of the client are associated to it
+     * a client and a controller of the client are associated to it.
+     * A new instance of CliUpdateHandler is created which manages
+     * all the updates notified by server.
      *
      * @param client indicates the client with whom a conversation with
      * the Cli instance created will take place.
@@ -79,7 +81,7 @@ public class Cli extends View implements Observer<Update> {
 
     /**
      * This is a method useful to avoid code repetitions.
-     * It simply prints a white line
+     * It simply prints a blank line
      */
     public void newLine() {
         if (stdout == null)
@@ -119,7 +121,13 @@ public class Cli extends View implements Observer<Update> {
         }
     }
 
-    void forwardNotify(Update update) { // forwards update to client-side Controller
+    /**
+     * This method forwards an update received to the
+     * client-side Controller
+     * @param update is the update received from Cli which is
+     *               forwarded to the Controller client-side.
+     */
+    void forwardNotify(Update update) {
         notify(update);
     }
 
@@ -133,6 +141,12 @@ public class Cli extends View implements Observer<Update> {
         this.isInitialGodChooser = value;
     }
 
+    /**
+     * This method is a simple getter to have access to the private
+     * instance of the boolean attribute isInitialGodChooser of this class.
+     * @return true if isInitialGodChooser is true value set,
+     *         otherwise it returns false.
+     */
     public boolean getInitialGodChooser() {
         return this.isInitialGodChooser;
     }
@@ -147,6 +161,12 @@ public class Cli extends View implements Observer<Update> {
         this.selectableGods = selectableGods;
     }
 
+    /**
+     * This is a method used to have a List that contains all the Gods a player can
+     * choose after GodChooser player selected them.
+     * @return a List with Gods references which are selectable from a
+     *         single player, during CHOOSE_GODS game phase.
+     */
     public List<String> getSelectableGods() {
         return this.selectableGods;
     }
@@ -159,6 +179,14 @@ public class Cli extends View implements Observer<Update> {
         this.selectedNicknames = selectedNicknames;
     }
 
+    /**
+     * This method is used to have a list that contains all the nicknames already
+     * chosen from different Players in a match. It is used in the first phase
+     * of a match, during INITIAL_INFO game phase, to avoid the possibility that
+     * different players involved in a match choose the same nicknames.
+     * @return a List with all the names already chosen from
+     *         other players for a single match.
+     */
     public List<String> getSelectedNicknames() {
         return this.selectedNicknames;
     }
@@ -171,6 +199,14 @@ public class Cli extends View implements Observer<Update> {
         this.selectableColors = selectableColors;
     }
 
+    /**
+     * This method is used to have a list that contains all the colors already
+     * chosen from different Players in a match. It is used in the first phase
+     * of a match, during INITIAL_INFO game phase, to avoid the possibility that
+     * different players involved in a match choose the same color.
+     * @return a List with all the colors already chosen from
+     *         other players for a single match.
+     */
     public List<PrintableColor> getSelectableColors() {
         return this.selectableColors;
     }
@@ -183,6 +219,11 @@ public class Cli extends View implements Observer<Update> {
         return this.playersNum;
     }
 
+    /**
+     * This method is a simple getter that set the number of the player involved in a match
+     * @param playersNum is an integer number that indicates how many player are involved
+     *                   in the match the particular Cli instance is involved in.
+     */
     public void setPlayersNum(int playersNum) {
         this.playersNum = playersNum;
     }
@@ -203,6 +244,13 @@ public class Cli extends View implements Observer<Update> {
         this.playersColors = playersColors;
     }
 
+    /**
+     * This method is a getter which gives a correspondence to the color chosen
+     * from a single player.
+     * @return an association between a String which contains the nickname
+     *         of a player involved and a PrintableColor that indicates a color
+     *         a player can choose during INITIAL_INFO game phase.
+     */
     public Map<String, PrintableColor> getPlayersColors() {
         return this.playersColors;
     }
@@ -258,67 +306,140 @@ public class Cli extends View implements Observer<Update> {
         this.playersColors.keySet().forEach((key) -> println(key + " is " + convertColorToAnsi(playersColors.get(key)) + playersColors.get(key) + PrintableColor.RESET));
     }
 
+    /**
+     * This method is used to print a specific nickname of a player
+     * involved in a match with the same color the player choose
+     * during the INITIAL_INFO game phase.
+     * @param nickname is a String which contains the nickname of a player
+     *                 involved in the match.
+     * @return a colored String. Colored with color chosen from the player with
+     *         the nickname given as parameter.
+     */
     public String playerWithColor(String nickname) {
-        if(playersColors != null) {
+        if(this.playersColors != null) {
 
-            if(!playersColors.containsKey(nickname) && nickname.equals("Player")) {
+            if(!this.playersColors.containsKey(nickname) && nickname.equals("Player")) {
                 return nickname;
             }
 
-            return convertColorToAnsi(playersColors.get(nickname)) + nickname + PrintableColor.RESET;
+            return convertColorToAnsi(this.playersColors.get(nickname)) + nickname + PrintableColor.RESET;
         }
         return null;
     }
 
+    /**
+     * This method is simply used as delegator. It is invoked from CliUpdateHandler
+     * when a match starts, a player's turn ends or when a player asks whose turn is it.
+     * It invokes otherInfoHandler.printCurrentTurn in which all the prints are defined.
+     */
     public void printCurrentTurn() {
         this.otherInfoHandler.printCurrentTurn();
     }
 
+    /**
+     * This method is used to print during GAME_PREPARATION game phase
+     * a particular string which invites Players to place them workers
+     * on the board.
+     */
     public void printGamePreparationInfo() {
         println("Game Preparation: place your " + Cli.toBold("workers") + ".    Command " + Cli.toBold("format") + " expected: place W1 [row1][col1]  W2 [row2][col2]");
         newLine();
     }
 
+    /**
+     * This method gives information about the current phase of the match
+     * @return the current phase of the match
+     */
     public GamePhase getCurrentPhase() {
         return this.currentGamePhase;
     }
 
+    /**
+     * This method is a simply getter. Once the player associated to the Cli
+     * lose, this method gives information if the wants to continue watch the match
+     * in which he was involved.
+     * @return true if the player response to the question
+     * "Do you want to continue to watch this match?" was positive, false otherwise.
+     */
     boolean wantsToContinueToWatch() {
         return this.continueToWatch;
     }
 
+    /**
+     * This method set continueToWatch attribute to the boolean value given
+     * as parameter. It indicates if a player which is involved in a match, after
+     * losing the match wants to watch other players play
+     * @param continueToWatch is a boolean value that indicates if the
+     *                        player wants continue to watch
+     */
     public void setContinueToWatch(boolean continueToWatch) {
         this.continueToWatch = continueToWatch;
     }
 
+    /**
+     * This method is used to make a string given as parameter in bold
+     * @param s is the string that you want in bold
+     * @return the bold String
+     */
     public static String toBold(String s) {
         return PrintableColor.BOLD + s + PrintableColor.RESET;
     }
 
+    /**
+     * This is the getter of the Client
+     * @return the Client associated to a particular instance of Cli
+     */
     public Client getClient(){
         return this.client;
     }
 
+    /**
+     * This method is used to have the JSON representation of the current Board.
+     * @return the String JSON-format of the board
+     */
     public String getCurrentBoard() {
         return this.currentBoard;
     }
 
+    /**
+     * This method is a simple setter of the String attribute currentBoard to the
+     * @param board given in JSON representation.
+     */
     public void setCurrentBoard(String board) {
         this.currentBoard = board;
     }
 
+    /**
+     * This method is a String getter which
+     * @return a particular string useful in many prints
+     */
     public String getCurrentPhaseString() {
         return "current_phase";
     }
 
+    /**
+     * This method prints the board. It is used when a player moves one of
+     * his workers, or build a level. It is also used when a player requests
+     * "BOARD" command.
+     * @param board is the JSON-format of the board to be printed.
+     */
     public void printBoard(String board) {
         this.gamePhaseCommandHandler.printBoard(board);
     }
 
+    /**
+     * This method makes available what is received as input from console.
+     * @return the Scanner stdin attribute in order to have access to monitor
+     *         what Cli receives as input in console.
+     */
     public Scanner getStdin() {
         return this.stdin;
     }
 
+    /**
+     * This method is a simple getter of the Controller client-side
+     * @return the instance of the Controller associated to Cli.
+     */
     public Controller getController() {
         return this.controller;
     }
