4fd059d056f99556d906ada6f70be71c8f4eda33
RobertoSpatafora <61706995+RobertoSpatafora@users.noreply.github.com>
2020-06-01 16:55:55 +0200

    Update MatchEnded.java

--- src/main/java/it/polimi/ingsw/view/cli/components/MatchEnded.java
+++ src/main/java/it/polimi/ingsw/view/cli/components/MatchEnded.java
@@ -5,13 +5,50 @@ import it.polimi.ingsw.view.cli.Cli;
 
 import java.io.IOException;
 
+/**
+ * This class deals with MATCH_ENDED game phase.
+ * It manages all the choices made by players. It allows players
+ * to start a new match once the match in which they were involved is finished.
+ */
 public class MatchEnded {
     private final Cli cli;
 
+    /**
+     * This is the constructor of this class. At the moment of the creation
+     * of a single instance of MatchEnded the cli associated to it is set
+     * @param cli contains reference to the Cli associated
+     */
     public MatchEnded(Cli cli){
         this.cli = cli;
     }
 
+    /**
+     * This method manages answers from user once the match ends.
+     * If a user type "yes" in this part it means that he wants to play another
+     * match. Otherwise, answering "no" to the question proposed, the user
+     * express its will to end.
+     * @param command contains the answer from the user
+     * @return true if the command received was "yes",
+     *         false otherwise.
+     */
+    public boolean handle(String command) {
+        if(command.equals("yes")) {
+            manage();
+            return true;        //break while in cli
+        }
+        else if(command.equals("no")) {
+            cli.println("Quitting...");
+            System.exit(0);
+        }
+        else throw new BadCommandException();
+        return false;
+    }
+
+    /**
+     * This private method manages the case in which a user answers
+     * "yes" to the question "Do you want to play another match?" once
+     * his match ended.
+     */
     private void manage(){
         try {
             cli.getClient().getUpdateListener().setIsActive(false);
@@ -26,18 +63,4 @@ public class MatchEnded {
         }
         cli.start();
     }
-
-    public boolean handle(String command) {
-        if(command.equals("yes")) {
-            manage();
-            return true;        //break while in cli
-        }
-        else if(command.equals("no")) {
-            cli.println("Quitting...");
-            System.exit(0);
-        }
-        else throw new BadCommandException();
-        return false;
-    }
-
 }
