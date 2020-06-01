4400c81622bb57c502344f2979c607cbddeb5bba
RobertoSpatafora <61706995+RobertoSpatafora@users.noreply.github.com>
2020-06-01 17:42:39 +0200

    Update JavaDoc

--- src/main/java/it/polimi/ingsw/view/cli/components/RealGame.java
+++ src/main/java/it/polimi/ingsw/view/cli/components/RealGame.java
@@ -3,14 +3,32 @@ package it.polimi.ingsw.view.cli.components;
 import it.polimi.ingsw.controller.commands.PlayerCommand;
 import it.polimi.ingsw.view.cli.Cli;
 
+/**
+ * This class manages REAL_GAME phase's commands.
+ *
+ * @author Andrea Vergani
+ * @author Roberto Spatafora
+ * @author Cosimo Sguanci
+ */
 public class RealGame {
 
     private final Cli cli;
 
+    /**
+     * This is the constructor of this class. At the moment of the creation
+     * of a single instance of RealGame the cli associated to it is set
+     * @param cli contains reference to the Cli associated
+     */
     public RealGame(Cli cli) {
         this.cli = cli;
     }
 
+    /**
+     * This method handles commands during REAL_GAME game phase.
+     * It makes a first parse of the input received from console and
+     * if it is a valid one it notifies it to the controller
+     * @param command contains a command received from console.
+     */
     public void handleRealGame(String command) {
         PlayerCommand playerCommand = PlayerCommand.parseInput(command);
         cli.notify(playerCommand);
--- src/main/java/it/polimi/ingsw/view/cli/components/WaitingForAMatch.java
+++ src/main/java/it/polimi/ingsw/view/cli/components/WaitingForAMatch.java
@@ -2,14 +2,32 @@ package it.polimi.ingsw.view.cli.components;
 
 import it.polimi.ingsw.view.cli.Cli;
 
+/**
+ * This class manages all the messages to print when a player
+ * waits for others to arrive.
+ *
+ * @author Andrea Vergani
+ * @author Roberto Spatafora
+ * @author Cosimo Sguanci
+ */
 public class WaitingForAMatch {
 
     private final Cli cli;
 
+    /**
+     * This is the constructor method of the class.
+     * It make an association between an instance of this class
+     * and the Cli associated to it.
+     * @param cli is the instance of Cli associated to GamePreparation.
+     */
     public WaitingForAMatch(Cli cli) {
         this.cli = cli;
     }
 
+    /**
+     * This method prints information to a player who is
+     * waiting for others to arrive.
+     */
     public void handleWaiting() {
         cli.println("Waiting for a match...");
         cli.newLine();
