69ebb78b8b592af28949959e1e088f57c32fce87
Cosimo Sguanci <cosimo.sguanci@mail.polimi.it>
2020-06-08 17:40:56 +0200

    fix Mockito fail with JDK 1.8

--- src/test/java/it/polimi/ingsw/model/updates/UpdateTest.java
+++ src/test/java/it/polimi/ingsw/model/updates/UpdateTest.java
@@ -5,62 +5,6 @@ import org.mockito.Mockito;
 
 public class UpdateTest {
     public static UpdateHandler getMockUpdateHandler() {
-
-        return Mockito.spy(new UpdateHandler() {
-            @Override
-            public void handle(GamePhaseUpdate update) {
-                System.out.println("GamePhaseUpdate");
-            }
-
-            @Override
-            public void handle(InitialInfoUpdate update) {
-                System.out.println("InitialInfoUpdate");
-            }
-
-            @Override
-            public void handle(MatchStartedUpdate update) {
-                System.out.println("MatchStartedUpdate");
-            }
-
-            @Override
-            public void handle(GodsUpdate update) {
-                System.out.println("GodsUpdate");
-            }
-
-            @Override
-            public void handle(BoardUpdate update) {
-                System.out.println("BoardUpdate");
-            }
-
-            @Override
-            public void handle(ErrorUpdate update) {
-                System.out.println("ErrorUpdate");
-            }
-
-            @Override
-            public void handle(TurnUpdate update) {
-                System.out.println("TurnUpdate");
-            }
-
-            @Override
-            public void handle(WinUpdate update) {
-                System.out.println("WinUpdate");
-            }
-
-            @Override
-            public void handle(LoseUpdate update) {
-                System.out.println("LoseUpdate");
-            }
-
-            @Override
-            public void handle(ServerUnreachableUpdate update) {
-                System.out.println("ServerUnreachableUpdate");
-            }
-
-            @Override
-            public void handle(DisconnectedPlayerUpdate update) {
-                System.out.println("DisconnectedPlayerUpdate");
-            }
-        });
+        return Mockito.mock(UpdateHandler.class);
     }
 }
