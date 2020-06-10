8eb43bf3c257e72a666da93cad4dd758fe818563
andmarver <andreamario.vergani@mail.polimi.it>
2020-06-10 19:22:54 +0200

    Client Gui merge

--- src/main/java/it/polimi/ingsw/view/gui/components/GodChoice.java
+++ src/main/java/it/polimi/ingsw/view/gui/components/GodChoice.java
@@ -19,6 +19,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -39,6 +40,8 @@ public class GodChoice extends JPanel implements ActionListener {
 
     private List<String> selectedGods;
     private List<String> selectableGods;
+
+    GodScreen godScreen;
 
     private Font titleFont = Gui.getFont(Gui.FONT_BOLD, 20);
     private Font godLabelFont = Gui.getFont(Gui.FONT_BOLD, 14);
@@ -126,196 +129,35 @@ public class GodChoice extends JPanel implements ActionListener {
     private void setSpecific() {
         ImageIcon startImg = new ImageIcon("src/main/resources/images/done.png");
         startImg = new ImageIcon(startImg.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH));
-        this.continueButton = new JRoundButton(startImg);
-        this.continueButton.setBorder(BorderFactory.createEmptyBorder(0,5, 0,5));
+        JRoundButton continueButton = new JRoundButton(startImg);
+        continueButton.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
+        continueButton.addActionListener(this);
 
         ImageIcon infoImg = new ImageIcon(this.standardImgPath + "information.png");
         infoImg = new ImageIcon(infoImg.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH));
-        this.infoButton = new JRoundButton(infoImg);
-        this.infoButton.setBorder(BorderFactory.createEmptyBorder(0,5, 0,5));
+        JRoundButton infoButton = new JRoundButton(infoImg);
+        infoButton.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
+        infoButton.addActionListener(new GodChoiceInfoButtonListener(this));
 
         boolean isGodChooser = this.gui.getController().isClientPlayerGodChooser();
-        if(isGodChooser) {
-            titleLabel.setText("Select " + playersNumber + " Gods");
-        }
-        else {
-            titleLabel.setText("Select a God");
+        if (isGodChooser) {
+            this.godScreen.setTitle("Select " + playersNumber + " Gods");
+        } else {
+            this.godScreen.setTitle("Select a God");
         }
     }
 
     private void addListeners() {
-        this.continueButton.addActionListener(this);
-        this.infoButton.addActionListener(new GodChoiceInfoButtonListener(this));
-
         boolean isGodChooser = this.gui.getController().isClientPlayerGodChooser();
         List<String> gods = isGodChooser ? View.getGodsNamesList() : this.selectableGods;
+        ArrayList<JButton> godButtons = this.godScreen.getButtons();
         for (int i = 0; i < gods.size(); i++) {
             this.godChoiceJButtonListener = new GodChoiceJButtonListener(this);
-            this.buttons.get(i).addActionListener(godChoiceJButtonListener);
+            godButtons.get(i).addActionListener(godChoiceJButtonListener);
         }
     }
 
-    private JRoundButton continueButton;
-    private JRoundButton infoButton;
-    private JLabel titleLabel = new JLabel();
-    private JPanel possibleGodsPanel = new JPanel();
-    private JPanel innerPanel = new JPanel();
-    private JPanel innerPanel2 = new JPanel();
 
-    /*private*/public void drawGodChoice(JPanel owner, Gui gui, JPanel possibleGodsPanel, JPanel innerPanel, JPanel innerPanel2, JLabel titleLabel, ArrayList<JButton> buttons) throws IOException {
-
-        boolean isGodChooser = /*this.*/gui.getController().isClientPlayerGodChooser();
-
-        LayoutManager layoutManager = new BorderLayout();
-        /*this.setLayout(layoutManager);*/
-        owner.setLayout(layoutManager);
-
-        JPanel titlePanel = new JPanel();
-        /*JPanel possibleGodsPanel = new JPanel();*/
-        /*ImageIcon startImg = new ImageIcon("src/main/resources/images/done.png");
-        startImg = new ImageIcon(startImg.getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH));
-
-        JRoundButton continueButton = new JRoundButton(startImg);
-        continueButton.addActionListener(this);*/
-        /*JPanel innerPanel = new JPanel();
-        JPanel innerPanel2 = new JPanel();/*
-
-        /*JLabel titleLabel;
-
-        if(isGodChooser) {
-            titleLabel = new JLabel("Select " + playersNumber + " Gods");
-        }
-        else {
-            titleLabel = new JLabel("Select a God");
-        }*/
-
-        //titleLabel = new JLabel();
-
-        titleLabel.setFont(this.titleFont);
-        titlePanel.add(titleLabel);
-        titlePanel.setBorder(BorderFactory.createEmptyBorder(20,0,20,0));
-        /*this.add(titlePanel, BorderLayout.NORTH);
-        this.setBorder(BorderFactory.createEmptyBorder(0,20,0,20));*/
-        owner.add(titlePanel, BorderLayout.NORTH);
-        owner.setBorder(BorderFactory.createEmptyBorder(0,20,0,20));
-
-
-       // GridLayout gridLayout = new GridLayout(4, 4);
-       // gridLayout.setVgap(10);
-        // possibleGodsPanel.setLayout(gridLayout); Uncomment for SINGLE BUTTONS GOD
-
-        GridLayout gridLayout = isGodChooser ? new GridLayout(4, 4) : new GridLayout(1, playersNumber);
-
-        possibleGodsPanel.setLayout(gridLayout);
-
-        ArrayList<ImageIcon> iconGods = new ArrayList<>();
-        ImageIcon imageIcon;
-
-        List<String> gods = isGodChooser ? View.getGodsNamesList() : this.selectableGods;
-
-        /*this.godChoiceJButtonListener = new GodChoiceJButtonListener(this);*/
-        for (int i = 0; i < gods.size(); i++) {
-            imageIcon = new ImageIcon(ImageIO.read(Gui.class.getResource("/images/GodChoice/" + gods.get(i).toLowerCase() + ".png")));
-            imageIcon = new ImageIcon(imageIcon.getImage().getScaledInstance(60,-1, Image.SCALE_SMOOTH));
-            iconGods.add(imageIcon);
-
-            JButton godBtn = new JButton(gods.get(i).toUpperCase(), iconGods.get(i));
-
-            godBtn.setFont(this.godLabelFont);
-            godBtn.setForeground(Color.BLACK); // Color.decode("0xc8102e")
-
-            godBtn.addComponentListener(new ComponentAdapter() {
-
-                @Override
-                public void componentResized(ComponentEvent e) {
-                    JButton btn = (JButton) e.getComponent();
-                    Dimension size = btn.getSize();
-
-                    String god = btn.getText().toLowerCase();
-
-                    if(size.width > 600 || size.height > 600) {
-                        btn.setIcon(allIconGods.get(god).get("very_big"));
-                    }
-
-                    else if(size.width > 400 || size.height > 400) {
-                        btn.setIcon(allIconGods.get(god).get("big"));
-                    }
-
-                    else if(size.width > 260 || size.height > 260) {
-                        btn.setIcon(allIconGods.get(god).get("medium_big"));
-                    }
-
-                    else if(size.width > 70 || size.height > 70) {
-                        btn.setIcon(allIconGods.get(god).get("medium"));
-                    }
-
-                    else if(size.width > 40 || size.height > 40) {
-                        btn.setIcon(allIconGods.get(god).get("medium_small"));
-                    }
-
-                    else {
-                        btn.setIcon(allIconGods.get(god).get("small"));
-
-                    }
-
-                }
-
-            });
-
-            /*this.*/buttons.add(godBtn);
-            //this.buttons.get(i).addActionListener(godChoiceJButtonListener);
-
-            godBtn.setHorizontalTextPosition(JButton.CENTER);
-            godBtn.setVerticalTextPosition(JButton.BOTTOM);
-            godBtn.setOpaque(false);
-            godBtn.setContentAreaFilled(false);
-
-            String operSys = System.getProperty("os.name").toLowerCase();
-
-            if(operSys.contains("mac")) {
-                godBtn.setBorderPainted(false);
-            }
-            else {
-                godBtn.setBorderPainted(true);
-            }
-            possibleGodsPanel.add(godBtn);
-
-
-          /*  JPanel pannel = new JPanel();
-            pannel.setLayout(new BoxLayout(pannel, BoxLayout.Y_AXIS));
-            godBtn.setAlignmentX(Component.CENTER_ALIGNMENT);
-            pannel.add(godBtn);
-
-            pannel.setOpaque(false);
-
-            possibleGodsPanel.add(pannel); */
-        }
-
-        //commonGodChoose(possibleGodsPanel, continueButton, innerPanel, innerPanel2);
-
-        titlePanel.setOpaque(false);
-        innerPanel.setOpaque(false);
-        innerPanel2.setOpaque(false);
-        possibleGodsPanel.setOpaque(false);
-    }
-
-    private void commonGodChoose(/*JPanel possibleGodsPanel, JButton continueButton, JPanel innerPanel, JPanel innerPanel2*/) {
-        this.add(possibleGodsPanel);
-
-        this.add(innerPanel, BorderLayout.SOUTH);
-        innerPanel.setLayout(new BorderLayout());
-        innerPanel2.setLayout(new BorderLayout());
-        JPanel boxLayout = new JPanel();
-        boxLayout.setLayout(new BoxLayout(boxLayout, BoxLayout.X_AXIS));
-        boxLayout.setOpaque(false);
-        boxLayout.add(infoButton);
-        boxLayout.add(Box.createHorizontalGlue());
-        boxLayout.add(continueButton);
-        innerPanel2.add(boxLayout, BorderLayout.EAST);
-        //innerPanel2.add(continueButton, BorderLayout.EAST);
-        innerPanel.add(innerPanel2);
-        innerPanel.setBorder(BorderFactory.createEmptyBorder(0,0,20,0));
-    }
 
     public void setGodChoiceSelected(JButton button) {
         if(gui.getController().isClientPlayerGodChooser()){
@@ -391,10 +233,12 @@ public class GodChoice extends JPanel implements ActionListener {
         this.removeAll();
 
         try {
-            drawGodChoice(this, this.gui, this.possibleGodsPanel, this.innerPanel, this.innerPanel2, this.titleLabel, this.buttons);
-            setSpecific();
-            commonGodChoose(/*possibleGodsPanel, continueButton, innerPanel, innerPanel2*/);
-            addListeners();
+            this.godScreen = new GodScreen(this.playersNumber, this.selectableGods, this.allIconGods);
+            this.add(this.godScreen);
+            //drawGodChoice(this, this.gui, this.possibleGodsPanel, this.innerPanel, this.innerPanel2, this.titleLabel, this.buttons);
+            //setSpecific();
+            //commonGodChoose(possibleGodsPanel, continueButton, innerPanel, innerPanel2);
+            //addListeners();
             this.revalidate();
             this.repaint();
         } catch(IOException e) {
--- src/main/java/it/polimi/ingsw/view/gui/components/GodInfo.java
+++ src/main/java/it/polimi/ingsw/view/gui/components/GodInfo.java
@@ -7,14 +7,5 @@ import java.awt.*;
 import java.io.IOException;
 
 public class GodInfo {
-/*
-    public static JDialog showAllGodsForInfo(JPanel owner) {
-        JDialog dialog = new JDialog((JFrame)null, "Information about Gods", false);
-        try{
-            owner.drawGodChoice(owner, Gui gui, JPanel possibleGodsPanel, JPanel innerPanel, JPanel innerPanel2, JLabel titleLabel, ArrayList<JButton> buttons)
-        } catch(IOException e) {
-            e.printStackTrace();
-        }
-    }
- */
+
 }
