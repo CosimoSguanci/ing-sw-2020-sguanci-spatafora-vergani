d89cf90dde54008610816a14792fed8bb199b827
andmarver <andreamario.vergani@mail.polimi.it>
2020-06-20 00:21:18 +0200

    JavaDoc for CommandType.java

--- src/main/java/it/polimi/ingsw/controller/commands/CommandType.java
+++ src/main/java/it/polimi/ingsw/controller/commands/CommandType.java
@@ -4,10 +4,16 @@ import it.polimi.ingsw.exceptions.BadCommandException;
 
 /**
  * CommandType is the enumeration for the type of command a player can do.
- * In Santorini there are three type of possible commands: MOVE used to
- * move a player's worker to a different cell; BUILD used to build in a cell
- * according to relatives restrictions, END_TURN to declare the termination
- * of the turn and then the turn pass to another player.
+ * In Santorini there are different type of possible commands: MOVE, used to
+ * move a player's worker to a different cell; BUILD, used to build in a cell
+ * according to its restrictions; END_TURN, to declare the end of the turn (and
+ * then the turn passes to another player); PICK, to select nickname and colour;
+ * SELECT, to choose a god/some gods; PLACE, to decide the initial position of the
+ * workers on the board; HELP, to receive some information about what to do; INFO,
+ * to have more specific information; QUIT, to leave a match; TURN, to "ask" who is
+ * playing his/her turn; RULES, to read a short version of Santorini's manual; GOD,
+ * to know the association player <-> god during a match; BOARD, to see the current
+ * board (and situation) of the match.
  *
  * @author Roberto Spatafora
  * @author Cosimo Sguanci
@@ -16,6 +22,15 @@ public enum CommandType {
 
     MOVE, BUILD, END_TURN, PICK, SELECT, PLACE, HELP, INFO, QUIT, TURN, RULES, GOD, BOARD;
 
+
+    /**
+     * This static method parses a string and sees if it corresponds to one of the CommandTypes.
+     * Matching takes place when the string is equal to a sort of .toString for CommandType; note
+     * that the string "end" is considered to match with CommandType.END_TURN.
+     *
+     * @return the matching command-type
+     *
+     */
     public static CommandType parseCommandType(String input) {
         try {
             return Enum.valueOf(CommandType.class, input.toUpperCase());
@@ -29,6 +44,15 @@ public enum CommandType {
         }
     }
 
+    /**
+     * This static method parses a string and sees if it corresponds to one of the CommandTypes of the
+     * "Help class"; this "class" is composed by all sort of CommandTypes that give information in order
+     * to help to play a better match.
+     * Matching takes place when the string is equal to a sort of .toString for CommandType.
+     *
+     * @return true if the string matches a CommandType of "Help class", false otherwise
+     *
+     */
     public static boolean isHelperCommandType(String command) {
         CommandType enumCommand;
         try {
