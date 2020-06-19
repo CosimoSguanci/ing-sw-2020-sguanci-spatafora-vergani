4b9c239995696a1e91c93066a7d5b6dd30f5cb09
RobertoSpatafora <61706995+RobertoSpatafora@users.noreply.github.com>
2020-06-19 21:00:20 +0200

    Javadoc GodInfo.java

--- src/main/java/it/polimi/ingsw/view/gui/components/GodInfo.java
+++ src/main/java/it/polimi/ingsw/view/gui/components/GodInfo.java
@@ -10,6 +10,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * GodInfo is a class used to give players information about God.
+ * This class gives players the possibility to have information
+ * about Gods. During GodChoice game phase players can click on info button
+ * and based on GodChooser or other they can have information about Gods.
+ * GodChooser has the possibility to read all the God's power by clicking on them,
+ * other players, by clicking on the info button, can see the power of Gods chosen
+ * from GodChooser for their match.
+ *
+ * @author Roberto Spatafora
+ * @author Andrea Vergani
+ */
 public class GodInfo extends JDialog {
 
     private final Gui gui = Gui.getInstance();
@@ -18,7 +30,15 @@ public class GodInfo extends JDialog {
     private final Image backgroundImage = new ImageIcon(getClass().getResource(godChoiceImgPath + "title_sky.png")).getImage();
     private GodScreen godScreen;
 
-
+    /**
+     * This method is the constructor of the class.
+     * At the moment of creation a list of String containing available God's names is given as parameter.
+     * By the strings received the constructor generates a JDialog with as many info about God buttons as
+     * the number of selectableGods.
+     * @param selectableGods is a list which contains selectable Gods names.
+     *                       Every String in list comports the presence of a button that
+     *                       if clicked it appears a JDialog with information about the power of that God
+     */
     public GodInfo(List<String> selectableGods) {
         super((Frame) null, "Information about Gods", false);
 
@@ -42,7 +62,7 @@ public class GodInfo extends JDialog {
             this.godScreen = new GodScreen(playersNumber, this.selectableGods);
             this.godScreen.setOpaque(false);
             //this.add(this.godScreen, BorderLayout.CENTER);
-            setSpecific();
+            this.godScreen.setTitle("Click on a God to have info about his/her powers!");
             addListeners();
             mainPanel.add(this.godScreen, BorderLayout.CENTER);
             this.add(mainPanel);
@@ -57,12 +77,10 @@ public class GodInfo extends JDialog {
 
     }
 
-
-    private void setSpecific() {
-        this.godScreen.setTitle("Click on a God to have info about his/her powers!");
-    }
-
-
+    /**
+     * This private method is used to add listeners to buttons that contains
+     * information about the power of a God in the component.
+     */
     private void addListeners() {
         boolean isGodChooser = this.gui.getController().isClientPlayerGodChooser();
         List<String> gods = isGodChooser ? View.getGodsNamesList() : this.selectableGods;
