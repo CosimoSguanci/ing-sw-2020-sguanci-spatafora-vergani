6ef53cc4a12877cbdcf7da476616f8945cb46ba2
andmarver <andreamario.vergani@mail.polimi.it>
2020-06-19 23:48:16 +0200

    JavaDoc for GamePhase.java

--- src/main/java/it/polimi/ingsw/controller/GamePhase.java
+++ src/main/java/it/polimi/ingsw/controller/GamePhase.java
@@ -4,13 +4,39 @@ import it.polimi.ingsw.exceptions.BadCommandException;
 
 import java.util.ArrayList;
 
+
+/**
+ * GamePhase is the enumeration for all match's phases. The main game-phases are: initial-info,
+ * when players decide a nickname and colour; choose-gods, when the god-chooser selects some gods
+ * and every player chooses the one he/she wants to play with; game-preparation, when every player
+ * places the workers on the board; real-game, when the real match is played; match-ended, the phase
+ * that follows a match; match-lost, phase for who lost a match.
+ *
+ * @author Andrea Mario Vergani
+ * @author Cosimo Sguanci
+ */
 public enum GamePhase {
     INITIAL_INFO, CHOOSE_GODS, GAME_PREPARATION, REAL_GAME, MATCH_ENDED, MATCH_LOST;
 
+
+    /**
+     * This static method simply returns the first phase of a match.
+     *
+     * @return the (chronological) first phase
+     *
+     */
     public static GamePhase firstPhase() {
         return INITIAL_INFO;
     }
 
+
+    /**
+     * This static method returns the phase that follows the parameter-phase.
+     *
+     * @param currentGamePhase the current game phase
+     * @return the (chronological) next phase
+     *
+     */
     public static GamePhase nextGamePhase(GamePhase currentGamePhase) {
         switch(currentGamePhase) {
             case INITIAL_INFO:
@@ -26,6 +52,14 @@ public enum GamePhase {
         }
     }
 
+    /**
+     * This static method parses a string and verifies if it represents a valid game-phase.
+     *
+     * @param input an input string, that should represent a game-phase
+     * @return true if the parameter is one of the GamePhases (hypothetical .toString), false
+     *         otherwise
+     *
+     */
     public static boolean isGamePhase(String input) {
         ArrayList<String> values = new ArrayList<>();
         for(GamePhase gamePhase : GamePhase.values()) {
@@ -35,6 +69,15 @@ public enum GamePhase {
         return values.contains(input.toLowerCase());
     }
 
+
+    /**
+     * This static method parses a string and verifies which of the valid game-phases it represents.
+     *
+     * @param input an input string, that should represent a game-phase
+     * @return the correspondent GamePhase if the parameter is one of the GamePhases
+     *         (hypothetical .toString)
+     *
+     */
     public static GamePhase parseGamePhase(String input) {
         try {
             return Enum.valueOf(GamePhase.class, input.toUpperCase());
@@ -43,7 +86,13 @@ public enum GamePhase {
         }
     }
 
-
+    /**
+     * This static method returns a StringBuilder containing all the game-phases that must be
+     * printed, in the form "gamePhase1, gamePhase2, ..."
+     *
+     * @return all the game-phases that must be printed, in the form "gamePhase1, gamePhase2, ..."
+     *
+     */
     public static StringBuilder toStringBuilder() {
         int j;
         GamePhase[] phases = GamePhase.class.getEnumConstants();
@@ -62,6 +111,13 @@ public enum GamePhase {
         return result;
     }
 
+    /**
+     * This static method verifies if the caller is a printable game-phase (so it must be printed
+     * as a string in some help command or something like this).
+     *
+     * @return true if the caller is printable, false otherwise
+     *
+     */
     public boolean isPrintable() {
         return this != MATCH_ENDED && this != MATCH_LOST;
     }
