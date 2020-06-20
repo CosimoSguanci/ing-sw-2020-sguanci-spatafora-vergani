344ef7e61a9efa60546f8de041e5a7822f1ed444
andmarver <andreamario.vergani@mail.polimi.it>
2020-06-20 12:40:33 +0200

    JavaDoc for PlayerCommand.java (updated and finished)

--- src/main/java/it/polimi/ingsw/controller/commands/PlayerCommand.java
+++ src/main/java/it/polimi/ingsw/controller/commands/PlayerCommand.java
@@ -8,10 +8,10 @@ import java.util.Arrays;
 import java.util.Map;
 
 /**
- * In this class there are references to the player who has playing
- * the current turn, the type of command requested from the player,
- * a reference to the worker the player wants it to move or build and
- * the cell in which the player wants the worker to move in or build in.
+ * In this class there are references to the player who is playing
+ * the current turn during the RealGame phase, the type of command requested by the player,
+ * the worker he/she wants to move or build with and
+ * the cell in which the player wants to move or build in.
  *
  * @author Roberto Spatafora
  * @author Cosimo Sguanci
@@ -22,7 +22,7 @@ public class PlayerCommand extends Command {
     public final String workerID;
 
     /**
-     * The Row/Column identifier that's used to identify the Cell in which the Player want to move its Worker,
+     * The Row/Column identifier that is used to identify the Cell in which the Player wants to move his/her Worker,
      * or where the Worker wants to build a new level.
      */
     public final int row;
@@ -38,8 +38,22 @@ public class PlayerCommand extends Command {
 
 
     /**
-     * PlayerCommand is the builder of the class. Taken as parameters
-     * the builder sets the class' attributes to the relatives values received.
+     * PlayerCommand is the constructor of the class. It sets the class' attributes
+     * such as the commandType (move, build, ...), the interested worker, the destination-cell
+     * of move/build and an optional level (in case of building a block).
+     *
+     * @param commandType the type of command, specific of RealGame phase
+     * @param workerID ID of worker to move/build with; null if the operation involves no
+     *                 worker (such as an end of turn)
+     * @param row an integer representing the row of cell where to move/build in; possible
+     *            values are from 0 to number of rows in Board minus one; -1 if the operation
+     *            involves no cell (such as an end of turn)
+     * @param col an integer representing the column of cell where to move/build in; possible
+     *            values are from 0 to number of columns in Board minus one; -1 if the
+     *            operation involves no cell (such as an end of turn)
+     * @param cellBlockType the type of block that the player wants to build; null if the
+     *                      operation is not a build, or the player wants to build the default
+     *                      level (so he/she wants to build just one level higher)
      */
     public PlayerCommand(CommandType commandType, String workerID, int row, int col, BlockType cellBlockType) {
         super(commandType);
@@ -49,22 +63,56 @@ public class PlayerCommand extends Command {
         this.cellBlockType = cellBlockType;
     }
 
+
+    /**
+     * This method is the setter for "cell" property, that must be reconstructed by server-side
+     * Controller.
+     *
+     * @param cell the cell where to move/build in
+     */
     public void setCell(Cell cell) {
         this.cell = cell;
     }
 
+
+    /**
+     * This method is the getter for "cell" property, that is reconstructed by server-side
+     * Controller.
+     *
+     * @return the cell where to move/build in
+     */
     public Cell getCell() {
         return this.cell;
     }
 
+
+    /**
+     * This method is the getter for "worker" property, that is reconstructed by server-side
+     * Controller.
+     *
+     * @return the worker that is going to move/build
+     */
     public Worker getWorker() {
         return this.worker;
     }
 
+
+    /**
+     * This method is the setter for "worker" property, that must be reconstructed by server-side
+     * Controller.
+     *
+     * @param worker the worker that is going to move/build
+     */
     public void setWorker(Worker worker) {
         this.worker = worker;
     }
 
+
+    /**
+     * This method handles the PlayerCommand received by Server in the proper way.
+     *
+     * @param handler the object that is going to handle the command
+     */
     @Override
     public void handleCommand(CommandHandler handler) {
         handler.handle(this);
@@ -74,8 +122,8 @@ public class PlayerCommand extends Command {
      * Static method used to parse a String command input by the user (CLI) into a PlayerCommand instance using String-rep for Workers.
      *
      * @param command  input String taken from Standard Input
-     * @return the corresponding PlayerCommand instance if the command was well formatted.
-     * @throws BadCommandException if the command String isn't well formatted.
+     * @return the corresponding PlayerCommand instance (if the command is well formatted)
+     * @throws BadCommandException if the command String is not well formatted
      */
     public static PlayerCommand parseInput(String command) throws BadCommandException {
 
