2668d7abeaea00ca09d617c3462d6a0e2702fe55
RobertoSpatafora <61706995+RobertoSpatafora@users.noreply.github.com>
2020-06-24 22:10:56 +0200

    JavaDoc GodListtener Gui

--- src/main/java/it/polimi/ingsw/view/gui/gods/listeners/AtlasListener.java
+++ src/main/java/it/polimi/ingsw/view/gui/gods/listeners/AtlasListener.java
@@ -11,12 +11,30 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * This class is a listener used for Atlas. It manages the case in
+ * which player using Atlas wants to build a DOME instead of another level.
+ *
+ * @author Roberto Spatafora
+ * @author Cosimo Sguanci
+ * @author Andrea Vergani
+ */
 public class AtlasListener extends GodListener implements ActionListener {
 
+    /**
+     * This is the creator of the class. At the moment of the creation
+     * the realComponent is associated to the class.
+     * @param realGameComponent references the RealGameComponent which class refers to.
+     */
     public AtlasListener(RealGame realGameComponent) {
         super(realGameComponent);
     }
 
+    /**
+     * This method manages the activities when the player with Atlas
+     * decides to use Atlas' power to build a dome.
+     * @param e contains a reference to the event of the listener.
+     */
     @Override
     public void actionPerformed(ActionEvent e) {
         JCellButton selectedWorkerButton = realGameComponent.getSelectedWorker();
--- src/main/java/it/polimi/ingsw/view/gui/gods/listeners/GodListener.java
+++ src/main/java/it/polimi/ingsw/view/gui/gods/listeners/GodListener.java
@@ -2,10 +2,22 @@ package it.polimi.ingsw.view.gui.gods.listeners;
 
 import it.polimi.ingsw.view.gui.components.RealGame;
 
+/**
+ * This abstract class is a generic GodListener.
+ *
+ * @author Roberto Spatafora
+ * @author Cosimo Sguanci
+ * @author Andrea Vergani
+ */
 public abstract class GodListener {
 
     protected RealGame realGameComponent;
 
+    /**
+     * This is the creator of the class. At the moment of a concrete class creation
+     * it is necessary to have a reference of the realGameComponent.
+     * @param realGameComponent contains a reference to the realGameComponent associated to gui
+     */
     protected GodListener(RealGame realGameComponent) {
         this.realGameComponent = realGameComponent;
     }
