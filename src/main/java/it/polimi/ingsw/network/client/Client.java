5a0ed83efffe8374a255e8988c8f0cc70e3c621a
andmarver <andreamario.vergani@mail.polimi.it>
2020-06-25 23:59:03 +0200

    JavaDoc for Client.java

--- src/main/java/it/polimi/ingsw/network/client/Client.java
+++ src/main/java/it/polimi/ingsw/network/client/Client.java
@@ -14,7 +14,16 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-
+/**
+ * Client class handles all what is related to network for client-side. In particular, this project
+ * is based on a distributed client-server approach through socket: client is the "place" from which
+ * a player sends commands to server, where he/she receives updates, ...; it connects to server's
+ * specific (IP,port) and shares appropriate objects with it.
+ *
+ * @author Andrea Mario Vergani
+ * @author Cosimo Sguanci
+ * @author Roberto Spatafora
+ */
 public class Client {
 
     //private final static String IP = "SantoriniServer-env.eba-idxatybv.us-east-1.elasticbeanstalk.com";
@@ -32,10 +41,24 @@ public class Client {
     private ObjectOutputStream objectOutputStream;
     private UpdateListener updateListener;
 
+    /**
+     * The constructor creates a Client object and initializes client-server socket connection.
+     * Server is supposed to be active on specified IP and PORT.
+     *
+     * @throws IOException if connection can't be established, for any reason (network error,
+     *                     unavailable server, ...)
+     */
     public Client() throws IOException {
         initConnection();
     }
 
+    /**
+     * This private method establishes effective connection between Client object and server.
+     * Streams for socket communication, both input and output, are created.
+     *
+     * @throws IOException if connection can't be established, for any reason (network error,
+     *                     unavailable server, ...)
+     */
     private void initConnection() throws IOException {
         this.socket = new Socket(IP, PORT);
         this.socket.setSoTimeout(TIMEOUT_MS);
@@ -44,10 +67,31 @@ public class Client {
         this.objectOutputStream = null;
     }
 
+
+    /**
+     * The method's task is to initialize a client-server socket connection from an already existing
+     * Client object; so, this method differs from constructor because the object exists. The method's
+     * main usage is that of establishing a new connection after closing one: in fact, after a match,
+     * a player can decide to play another match; in this case, this method is called, in order to have
+     * a new connection to server.
+     *
+     * @throws IOException if connection can't be established, for any reason (network error,
+     *                     unavailable server, ...)
+     */
     public void reinitializeConnection() throws IOException {
         initConnection();
     }
 
+
+    /**
+     * This method initializes listener for updates coming from Server; in fact, client and
+     * server send/receive objects through socket. In this method, pong message sender is created
+     * too, and it becomes active: this allows server to control if all clients are connected (if
+     * not, the match must immediately end).
+     *
+     * @throws IOException if an I/O error occurs when creating the output stream or if the socket
+     * is not connected
+     */
     public void setupUpdateListener() throws IOException {
         if (this.objectOutputStream == null) {
             this.objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
@@ -58,11 +102,28 @@ public class Client {
         pongScheduler.scheduleAtFixedRate(new PingSender(this.objectOutputStream, this.pongScheduler), 0, PONG_SCHEDULE_TIME_MS, TimeUnit.MILLISECONDS);
     }
 
+
+    /**
+     * This method sends to server the number of players selected for the match. A Santorini match
+     * can be played by 2 or 3 people, and every player decides, before the match itself, his/her
+     * opponent's number.
+     *
+     * @param playersNum number of players of the match: must be 2 or 3
+     * @throws IOException if an I/O error occurs
+     */
     public void sendPlayersNumber(int playersNum) throws IOException {
         dataOutputStream.writeInt(playersNum);
         dataOutputStream.flush();
     }
 
+
+    /**
+     * This method simply returns a String representing the unique ID given to player by server.
+     *
+     * @return unique player ID given by server
+     * @throws IOException if an I/O error occurs when doing some of the operations (like reading
+     *                     or closing socket, if server is unavailable)
+     */
     public String readPlayerID() throws IOException {
         while (true) {
             try {
@@ -77,6 +138,13 @@ public class Client {
         throw new IOException();
     }
 
+
+    /**
+     * The method sends a Command, generated client-side, to server. In general, Client sends
+     * Commands to Server, while receives Updates from it.
+     *
+     * @param command the command to send to server, through socket
+     */
     public void sendCommand(Command command) {
 
         try {
@@ -94,11 +162,23 @@ public class Client {
 
     }
 
-
+    /**
+     * This method is the getter for UpdateListener of Client Class; UpdateListener is Client's
+     * listener, always ready to receive objects form Server.
+     *
+     * @return updateListener attribute of Client class
+     */
     public UpdateListener getUpdateListener() {
         return this.updateListener;
     }
 
+    /**
+     * The method checks whether server is reachable through network (socket) or not. Remind that
+     * server is supposed to be "located" at (IP,PORT) attributes.
+     *
+     * @return true if server is reachable, so a socket connection with servercan be established;
+     *         false otherwise
+     */
     private boolean isServerReachable() {
         try {
             Socket socket = new Socket(IP, PORT);
